828d6657a4cb5d801c4881d26fd707a54c011612
OleksiiMartseniuk <martsenyk.l@gmail.com>
2023-01-23 21:51:35 +0200

    add settings token bot

--- src/config.py
+++ src/config.py
@@ -15,3 +15,6 @@ POSTGRES_USER = os.getenv("POSTGRES_USER", "postgres")
 POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD", "postgres")
 POSTGRES_HOST = os.getenv("POSTGRES_HOST", "127.0.0.1")
 POSTGRES_PORT = os.getenv("POSTGRES_PORT", "5432")
+
+# Telegram
+TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN", "token")
