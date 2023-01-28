16337dd0c2a6811bd4b4680ea8ae1550d476550a
OleksiiMartseniuk <martsenyk.l@gmail.com>
2023-01-28 15:24:10 +0200

    edit menu

--- src/bot/handlers/menu_program.py
+++ src/bot/handlers/menu_program.py
@@ -1,9 +1,15 @@
+import logging
+
 from collections import defaultdict
 
 from aiogram import Dispatcher, types
 
+from config import MEDIA_ROOT
 from programs import service
 from bot.keyboard.inline import menu_keyboard
+
+
+logger = logging.getLogger(__name__)
 
 
 async def program_start(message: types.Message):
@@ -23,7 +29,11 @@ async def list_category(
         await callback.message.edit_text(text, reply_markup=markup)
 
 
-async def list_programs(callback: types.CallbackQuery, category, **kwargs):
+async def list_programs(
+    callback: types.CallbackQuery,
+    category: int,
+    **kwargs
+):
     markup = await menu_keyboard.program_keyboard(category)
     await callback.message.edit_text(
         "Выберите программу:",
@@ -31,10 +41,15 @@ async def list_programs(callback: types.CallbackQuery, category, **kwargs):
     )
 
 
-async def exercises(callback: types.CallbackQuery, category, program):
-    markup = await menu_keyboard.exercises_keyboard(category, program)
+async def list_day(
+    callback: types.CallbackQuery,
+    category: int,
+    program: int,
+    **kwargs
+):
+    markup = await menu_keyboard.day_keyboard(category, program)
 
-    exercises = await service.get_exercises(program_id=program)
+    exercises = await service.get_exercises_list(program_id=program)
     day_week = defaultdict(list)
     for e in exercises:
         day_week[e.day].append(
@@ -52,15 +67,76 @@ async def exercises(callback: types.CallbackQuery, category, program):
     )
 
 
+async def list_exercises(
+    callback: types.CallbackQuery,
+    category: int,
+    program: int,
+    day: str,
+    **kwargs
+):
+    markup = await menu_keyboard.exercises_all_keyboard(category, program, day)
+    text = f"<b>{day.capitalize()}</b>"
+    await callback.message.edit_text(
+        text,
+        reply_markup=markup,
+        parse_mode=types.ParseMode.HTML
+    )
+
+
+async def get_exercises(
+    callback: types.CallbackQuery,
+    category: int,
+    program: int,
+    day: str,
+    exercises: int
+):
+    markup = await menu_keyboard.exercises_keyboard(
+        category, program, day, exercises
+    )
+    # TODO fix image
+    exercises_data = await service.get_exercises(exercises)
+    if exercises_data:
+        text = f"<b>{exercises_data.title.capitalize()}</b>\n"\
+              f"Количество подходов [{exercises_data.number_approaches}]\n"\
+              f"Количество повторений [{exercises_data.number_repetitions}]\n"\
+              f"Image {exercises_data.image}"
+        image = open(f"{MEDIA_ROOT}/{exercises_data.image}", "rb")
+        image_file = types.InputFile(image)
+        logger.info(image_file)
+        print(image_file)
+        media = types.InputMediaPhoto(media=image_file, caption="asdsad")
+        print(media)
+    else:
+        logger.error(
+            "Not exist exercises [category=%s, program=%s,"
+            " day=%s, exercises=%s]",
+            category,
+            program,
+            day,
+            exercises
+        )
+        text = "Не найдено упражнения"
+    # await callback.message.answer_photo(media)
+    await callback.message.edit_text(
+        text,
+        reply_markup=markup,
+        parse_mode=types.ParseMode.HTML
+    )
+
+
 async def navigate(call: types.CallbackQuery, callback_data: dict):
     current_level = callback_data.get("level", "0")
     category = callback_data.get("category", 0)
     program = callback_data.get("program", 0)
+    day = callback_data.get("day", "")
+    exercises = callback_data.get("exercises", 0)
 
     levels = {
         "0": list_category,
         "1": list_programs,
-        "2": exercises
+        "2": list_day,
+        "3": list_exercises,
+        "4": get_exercises
     }
 
     current_level_function = levels[current_level]
@@ -68,7 +144,9 @@ async def navigate(call: types.CallbackQuery, callback_data: dict):
     await current_level_function(
         call,
         category=int(category),
-        program=int(program)
+        program=int(program),
+        day=day,
+        exercises=int(exercises)
     )
 
 
