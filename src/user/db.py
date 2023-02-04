63f49e9fbe4f26d127d928b03681dde830b46780
OleksiiMartseniuk <martsenyk.l@gmail.com>
2023-02-04 10:58:33 +0200

    add log to get user

--- src/user/db.py
+++ src/user/db.py
@@ -1,9 +1,14 @@
+import logging
+
 from sqlalchemy import insert, select
 
 from database import async_session
 
 from .models import user
 from . import schemas
+
+
+logger = logging.getLogger(__name__)
 
 
 async def create_user(
@@ -28,4 +33,8 @@ async def get_user(telegram_id: int) -> schemas.User | None:
     async with async_session() as session:
         result = await session.execute(query)
         user_data = result.fetchone()
-        return schemas.User(*user_data) if user_data else None
+        if user_data:
+            return schemas.User(*user_data)
+        else:
+            logger.error("User telegram id %s not exist.", telegram_id)
+            return None
