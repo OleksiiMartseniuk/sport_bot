3a94e074f7a1affac834e2fb0b101084a0cc8434
Oleksii Martseniuk <martsenyk.l@gmail.com>
2023-02-09 22:10:15 +0200

    add get_current_statistic

--- src/statistic/service.py
+++ src/statistic/service.py
@@ -1,8 +1,10 @@
 import logging
 
+from prettytable import PrettyTable
 from datetime import datetime
 
 from user import db as user_db
+from programs import db as program_db
 
 from . import db as statistic_db
 
@@ -98,3 +100,50 @@ async def set_statistics_exercises(
         done=done,
         created=created
     )
+
+
+async def get_current_statistic(
+        telegram_user_id: int,
+        program_id: int
+) -> str | None:
+    user = await user_db.get_user(telegram_id=telegram_user_id)
+    if not user:
+        return
+
+    statistics_program = await statistic_db.check_active_statistics_program(
+        user_id=user.id,
+        program_id=program_id
+    )
+
+    if not statistics_program:
+        logger.info(f"Not exist active program in user {user.id}")
+        return "<b>У вас не существует активной программы!!!</b>"
+
+    program = await program_db.get_program(id=program_id)
+    if not program:
+        logger.error(f"Program not exist program_id {program_id}")
+        return None
+
+    statistics_exercises_list = await statistic_db.get_list_exercises(
+        program_id=program.id
+    )
+
+    table = PrettyTable()
+    table.field_names = ["Упражнения", "Выполнения", "Дата"]
+    for exercises_stc in statistics_exercises_list:
+        exercises = await program_db.get_exercises(
+            exercises_id=exercises_stc.exercises_id
+        )
+        if not exercises:
+            logger.error(f"Not exist program {exercises_stc.exercises_id}")
+            continue
+
+        table.add_row([
+            exercises.title,
+            "✅" if exercises_stc.done else "❌",
+            exercises_stc.created.strftime("%Y-%m-%d %H:%M")
+        ])
+
+    return f"<b>{program.title}</b> "\
+           f"{statistics_program.start_time.strftime('%Y-%m-%d %H:%M')}\n"\
+           f" <pre>{table}</pre>"
