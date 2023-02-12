b79920dc7ef26bbec9e7d69c34b09fdab363637b
Oleksii Martseniuk <martsenyk.l@gmail.com>
2023-02-12 21:05:00 +0200

    register base handler

--- src/main.py
+++ src/main.py
@@ -6,7 +6,10 @@ from aiogram.contrib.fsm_storage.redis import RedisStorage2
 from bot.handlers.menu_program import register_handlers_program
 from bot.handlers.menu_statistic import register_handlers_statistic
 from bot.handlers.import_file import register_handlers_import_file
+from bot.handlers.base import register_handlers_base
+
 from bot.handlers.commands import setup_bot_commands
+
 from bot.middlewares.throttling import ThrottlingMiddleware
 from bot.middlewares.registration import RegistrationMiddleware
 from bot.middlewares.permission import PermissionMiddleware
@@ -29,6 +32,7 @@ dp.middleware.setup(RegistrationMiddleware())
 dp.middleware.setup(PermissionMiddleware())
 
 # Register Handlers
+register_handlers_base(dp)
 register_handlers_program(dp)
 register_handlers_statistic(dp)
 register_handlers_import_file(dp)
